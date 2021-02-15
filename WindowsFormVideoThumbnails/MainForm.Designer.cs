2e1ba3abe3994c1b9b99ad745a8fd87ce1c7ef0f
Amador Flores <jamadorfi@gmail.com>
2021-02-14 22:35:11 -0600

    Add project files.

---
+++ WindowsFormVideoThumbnails/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormVideoThumbnails
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.explorerTextBox = new System.Windows.Forms.TextBox();
+            this.chooseFolderBtn = new System.Windows.Forms.Button();
+            this.filesListBox = new System.Windows.Forms.ListBox();
+            this.listFilesBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startDateTimePicker
+            // 
+            this.startDateTimePicker.Location = new System.Drawing.Point(84, 85);
+            this.startDateTimePicker.Name = "startDateTimePicker";
+            this.startDateTimePicker.Size = new System.Drawing.Size(285, 20);
+            this.startDateTimePicker.TabIndex = 0;
+            // 
+            // endDateTimePicker
+            // 
+            this.endDateTimePicker.Location = new System.Drawing.Point(412, 85);
+            this.endDateTimePicker.Name = "endDateTimePicker";
+            this.endDateTimePicker.Size = new System.Drawing.Size(300, 20);
+            this.endDateTimePicker.TabIndex = 1;
+            // 
+            // explorerTextBox
+            // 
+            this.explorerTextBox.Enabled = false;
+            this.explorerTextBox.Location = new System.Drawing.Point(84, 41);
+            this.explorerTextBox.Name = "explorerTextBox";
+            this.explorerTextBox.Size = new System.Drawing.Size(461, 20);
+            this.explorerTextBox.TabIndex = 2;
+            // 
+            // chooseFolderBtn
+            // 
+            this.chooseFolderBtn.Location = new System.Drawing.Point(551, 41);
+            this.chooseFolderBtn.Name = "chooseFolderBtn";
+            this.chooseFolderBtn.Size = new System.Drawing.Size(161, 23);
+            this.chooseFolderBtn.TabIndex = 3;
+            this.chooseFolderBtn.Text = "Selecciona Carpeta";
+            this.chooseFolderBtn.UseVisualStyleBackColor = true;
+            this.chooseFolderBtn.Click += new System.EventHandler(this.chooseFolderBtn_Click);
+            // 
+            // filesListBox
+            // 
+            this.filesListBox.FormattingEnabled = true;
+            this.filesListBox.Location = new System.Drawing.Point(84, 167);
+            this.filesListBox.Name = "filesListBox";
+            this.filesListBox.Size = new System.Drawing.Size(628, 121);
+            this.filesListBox.TabIndex = 4;
+            // 
+            // listFilesBtn
+            // 
+            this.listFilesBtn.Location = new System.Drawing.Point(84, 123);
+            this.listFilesBtn.Name = "listFilesBtn";
+            this.listFilesBtn.Size = new System.Drawing.Size(628, 23);
+            this.listFilesBtn.TabIndex = 5;
+            this.listFilesBtn.Text = "Listar Videos";
+            this.listFilesBtn.UseVisualStyleBackColor = true;
+            this.listFilesBtn.Click += new System.EventHandler(this.listFilesBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(84, 312);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(144, 23);
+            this.convertBtn.TabIndex = 6;
+            this.convertBtn.Text = "Extraer imágenes";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.listFilesBtn);
+            this.Controls.Add(this.filesListBox);
+            this.Controls.Add(this.chooseFolderBtn);
+            this.Controls.Add(this.explorerTextBox);
+            this.Controls.Add(this.endDateTimePicker);
+            this.Controls.Add(this.startDateTimePicker);
+            this.AccessibleName = "MainForm";
+            this.Text = "Exportador de videos 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker startDateTimePicker;
+        private System.Windows.Forms.DateTimePicker endDateTimePicker;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox explorerTextBox;
+        private System.Windows.Forms.Button chooseFolderBtn;
+        private System.Windows.Forms.ListBox filesListBox;
+        private System.Windows.Forms.Button listFilesBtn;
+        private System.Windows.Forms.Button convertBtn;
+    }
+}
+
---
+++ WindowsFormVideoThumbnails/MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormVideoThumbnails
+{
+    public partial class MainForm : Form
+    {
+        private const string DATA_URI = "data.xml";
+        private const string Name = "directoryFolder";
+        private FileInfo[] _filesInfo = default;
+        private string _directoryName = default;
+        private XDocument _xmlData = default;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                _xmlData = XDocument.Load(DATA_URI);
+            }
+            catch (Exception)
+            {
+                var xmlDocument = new XDocument(new XElement(Name, ""));
+                xmlDocument.Save(DATA_URI);
+                _xmlData = XDocument.Load(DATA_URI);
+            }
+        }
+
+        private void chooseFolderBtn_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog.Description = "Seleccione la carpeta dónde se encuentren todas las grabaciones";
+            if (this.folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.explorerTextBox.Text = this.folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void listFilesBtn_Click(object sender, EventArgs e)
+        {
+            if (this.explorerTextBox.Text.Any())
+            {
+                ClearFiles();
+                ListFiles();
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona una carpeta para encontrar los videos dentro de ella.");
+            }
+        }
+
+        private void ClearFiles()
+        {
+            this.filesListBox.Items.Clear();
+            this._filesInfo = default;
+        }
+
+        private void ListFiles()
+        {
+            var startDate = startDateTimePicker.Value.Date;
+            var endDate = endDateTimePicker.Value.Date.AddDays(1).AddTicks(-1);
+            this._directoryName = String.Format("{0} - {1}", startDate.ToString("dd-MM-yyyy"), endDate.ToString("dd-MM-yyyy"));
+            var directoyInfo = new DirectoryInfo(this.explorerTextBox.Text);
+            this._filesInfo = directoyInfo.GetFiles("*.mp4", SearchOption.AllDirectories)
+                                        .Where(x => x.CreationTime > startDate && x.CreationTime < endDate).ToArray();
+
+            if (this._filesInfo.Any())
+            {
+                this.filesListBox.BeginUpdate();
+                foreach (var fileInfo in _filesInfo)
+                {
+                    this.filesListBox.Items.Add(String.Format("{0} - {1}", fileInfo.Name, fileInfo.CreationTime.ToString("dddd, dd MMMM yyyy hh:mm tt")));
+
+                }
+                this.filesListBox.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("No se encontró ningún video con las fechas y carpeta seleccionada.");
+            }
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            if (this._filesInfo != default && this._filesInfo.Any())
+            {
+                var fmmpeg = new NReco.VideoConverter.FFMpegConverter();
+                this.folderBrowserDialog.Description = "Seleccione donde quiere guardar las imágenes de los videos";
+                if (this.folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var path = this.folderBrowserDialog.SelectedPath;
+                    DirectoryInfo directoryInfo = default;
+                    foreach (var fileInfo in this._filesInfo)
+                    {
+                        directoryInfo = Directory.CreateDirectory(String.Format("{0}/{1}", path, _directoryName));
+                        fmmpeg.GetVideoThumbnail(fileInfo.FullName, String.Format("{0}/{1}.jpg", directoryInfo.FullName, fileInfo.Name.Replace(".mp4", String.Empty)), 2f);
+                    }
+
+                    if (MessageBox.Show(String.Format("Imágenes creadas con éxito en {0}", directoryInfo.FullName)) == DialogResult.OK)
+                    {
+                        Process.Start(directoryInfo.FullName);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor seleccione carpeta de las grabaciones, fechas y click en listar videos, se necesita al menos un video para poder obtener sus imágenes");
+            }
+        }
+    }
+}
